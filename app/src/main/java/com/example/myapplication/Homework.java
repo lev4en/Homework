e8ec239fa89732eff7d173f631995df975478bd1
Nikita Levchenko <lev4enko99@gmail.com>
2022-11-13 17:11:29 +0300

    Revert "Выполнил задание 1."
    This reverts commit c4585ed88b4c2ff21f048c227b1b79da8e5764d5.

--- app/src/main/java/com/example/myapplication/Homework.java
+++ app/src/main/java/com/example/myapplication/Homework.java
@@ -1,21 +1,7 @@
 package com.example.myapplication;
 
-import java.util.Scanner;
-
 public class Homework {
     public static void main(String[] args){
-        Scanner in = new Scanner(System.in);
-        System.out.println("Введите число для проверки на четность");
-        int a = in.nextInt();
-        Zadanie_1(a);
-    }
-    static void Zadanie_1(int a){
-        int x= a % 2;
-        if (x ==  0) {
-            System.out.println("Это число четное");
-        }
-        else{
-            System.out.println("Это число нечетное");
-        }
+
     }
 }
