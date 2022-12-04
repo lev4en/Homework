842c6340ea411a53e6239b0a5493531c59502f3d
Nikita Levchenko <lev4enko99@gmail.com>
2022-12-04 19:14:37 +0300

    Revert "старое"
    This reverts commit 4b522ad3f5cc35c8cd2941ee588e4af2c823efe7.

--- app/src/main/java/lesson9/practice/Maker.java
+++
@@ -1,9 +0,0 @@
-package lesson9.practice;
-
-public class Maker {
-    public static void throwOne() throws IndexOutOfBoundsException{
-        System.out.println("внутри throwOne");
-        throw new IndexOutOfBoundsException("Ошибка такая-то");
-        }
-    }
-
