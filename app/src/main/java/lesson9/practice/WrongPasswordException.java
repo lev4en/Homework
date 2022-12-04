e9c4e1c109d34c84e947b990978b705dbac0e0df
Nikita Levchenko <lev4enko99@gmail.com>
2022-12-04 19:13:47 +0300

    Revert "хомеворк"
    This reverts commit 3c1eb4042b223d4f0634dc5c50bfe3684073f340.

--- app/src/main/java/lesson9/practice/WrongPasswordException.java
+++
@@ -1,9 +0,0 @@
-package lesson9.practice;
-
-public class WrongPasswordException extends Exception {
-    public WrongPasswordException(){
-    }
-    public  WrongPasswordException(String message){
-        super(message);
-    }
-}
